6e89e05ff24329d4de28c7c8c6879d03c71ad3ce
technero <nermeen.hamed86@gmail.com>
2022-10-19 12:50:10 -0700

    correct the variables

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-const qwerty = getElementById(qwerty);
-const phrase = getElementById(phrase);
-const btnReset = document.querySelector(".btn_reset");
+const qwerty = document.getElementById("qwerty");
+const phrase = document.getElementById("phrase");
+const btnReset = document.getElementsByClassName("btn_reset");
+
+const missed_guesses = '';
+for (let i = 0; i > missed_guesses.length; i++) {
+    if (missed_guesses === 5) {
+        console.log = ("you are loser ");
+
+    };
+};
