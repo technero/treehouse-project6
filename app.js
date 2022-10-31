5f05cf512977957400a3aa422fadc81914b6733f
technero <nermeen.hamed86@gmail.com>
2022-10-30 23:12:03 -0700

    fix the getRandomPhrase function

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
 const qwerty = document.getElementById("qwerty");
 const phrase = document.getElementById("phrase");
-const btnReset = document.querySelector(".btn_reset");
+const btnReset = document.querySelector(".btn__reset");
 const overlay = document.getElementById("overlay");
 
 const missed_guesses = 0;
 
 const phrases = ["L", " M ", " A", " E", " K"];
-overlay.addEventListener("click", () => {
+btnReset.addEventListener("click", () => {
     overlay.style.display = "none";
 });
 //creating the stub function//
-function stub() {
-    const randomNumber = phrases.length;
+function getRandomPhraseAsArray(arr) {
+    const selectedNumber = Math.floor(Math.random() * arr.length);
+    return arr[selectedNumber];
+};
+
+getRandomPhraseAsArray(phrases);
+
+
+
+function addPhraseToDisplay() {
+
+    for (let i = 0; i < phrases[0].length; i++) {
+
+
+    };
+
+
 };
